961f9c24c291078f461800df7c94007aa19f0909
Josue Belacy <jbelacy@gmail.com>
2022-08-10 03:34:14 -0400

    integrated chart.js into example

--- js/app.js
+++ js/app.js
@@ -7,9 +7,69 @@ let state =  {
   home_insurance: document.querySelectorAll('[name="home_insurance"]')[0].value,
   hoa: document.querySelectorAll('[name="hoa"]')[0].value
 }
+let totalLoan, 
+totalMonths,
+monthlyInterest,
+monthlyPrincipalInterest,
+monthlyPropertyTaxes,
+monthlyHomeInsurance,
+monthlyHOA,
+
+labels = ["Principal & Interest", "Property Tax", "Home Insurance", "HOA"],
+  backgroundColor = [
+    "rgba(255, 99, 132, 1)",
+    "rgba(54, 162, 235, 1)",
+    "rgba(255, 206, 86, 1)",
+    "rgba(75, 192, 192, 1)",
+    "rgba(153, 102, 255, 1)",
+    "rgba(255, 159, 64, 1)",
+  ]
+  borderColor = [
+    "rgba(255, 99, 132, 1)",
+    "rgba(54, 162, 235, 1)",
+    "rgba(255, 206, 86, 1)",
+    "rgba(75, 192, 192, 1)",
+    "rgba(153, 102, 255, 1)",
+    "rgba(255, 159, 64, 1)",
+  ];
 
 function getNumber(string) {
   return Number(string.replace(/[^0-9\.-]+/g, ""))
-}
+} console.log(state);
 
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 console.log(state)
