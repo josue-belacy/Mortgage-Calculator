2c89be42c7d4c9488e6db098e5039fa6a57ce675
Josue Belacy <jbelacy@gmail.com>
2022-08-10 02:57:59 -0400

    Put values in state object

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let state =  {
+  price: document.querySelectorAll('[name="price"]')[0].value,
+  loan_years: document.querySelectorAll('[name="loan_years"]')[0].value,
+  down_payment: document.querySelectorAll('[name="down_payment"]')[0].value,
+  interest_rate: document.querySelectorAll('[name="interest_rate"]')[0].value,
+  property_tax: document.querySelectorAll('[name="property_tax"]')[0].value,
+  home_insurance: document.querySelectorAll('[name="home_insurance"]')[0].value,
+  hoa: document.querySelectorAll('[name="hoa"]')[0].value
+}
+
+
+console.log(state)
