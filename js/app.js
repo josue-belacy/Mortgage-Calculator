1c3448008a7ed77437f2400e005b0c70bb149cdf
Josue Belacy <jbelacy@gmail.com>
2022-08-10 05:25:55 -0400

    changed to pie chart

--- js/app.js
+++ js/app.js
@@ -39,9 +39,9 @@ function getNumber(string) {
 
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: labels,
         datasets: [{
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3],
